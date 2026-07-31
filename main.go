84686a77b0954e829c5aa458c94d98c7c5a09538
Robert Groenewegen <robert.groenewegen@gmail.com>
2026-07-31 19:45:09 +0200

    exits on repos without commits

--- main.go
+++ main.go
@@ -21,8 +21,8 @@ func main() {
 	}
 
 	// Start the HTTP server
-	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	log.Println("Starting server on port", cfg.port)
+	if err := http.ListenAndServe(":"+cfg.port, mux); err != nil {
 		log.Fatalf("start server: %v", err)
 	}
 }
