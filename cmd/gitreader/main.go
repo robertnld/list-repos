13c35bae60708a54fad78fa8af387bd95a6a8c9d
Robert Groenewegen <robert.groenewegen@gmail.com>
2026-08-28 19:44:59 +0200

    from struct to variables for parameters

--- cmd/gitreader/main.go
+++ cmd/gitreader/main.go
@@ -5,35 +5,21 @@ import (
 	"list-repos/gitreader"
 )
 
-// Structure for incoming parameters
-type config struct {
-	// The directory to scan for repositories
-	ListDir string `default:"."`
-}
-
 
 func main() {
-	cfg := config{}
-	flag.StringVar(&cfg.ListDir, "list-dir", ".", "The directory to scan for repositories")
+	// Parameters
+	listdir := flag.String("list-dir", ".", "The directory to scan for repositories")
 	flag.Parse()
 
-	// Print the banner message as test
-	line, err := gitreader.Banner()
-	if err != nil {
-		println("Error:", err.Error())
-		return
-	}
-	println(line)
-
 	// Check if the specified directory is a Git repository
-	if gitreader.IsGitRepository(cfg.ListDir) {
+	if gitreader.IsGitRepository(*listdir) {
 		println("This is a Git repository.")
 	} else {
 		println("This is NOT a Git repository.")
 	}
 
 	// Get the latest commit message for the Git repository at the specified path
-	commit_message, err := gitreader.GetLatestCommitMessage(cfg.ListDir)
+	commit_message, err := gitreader.GetLatestCommitMessage(*listdir)
 	if err != nil {
 		println("Error:", err.Error())
 		return
